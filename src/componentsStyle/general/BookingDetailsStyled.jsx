78d021c454076d103111be130f244c365a610d3c
omarPerez1987 <omar.perez.dev@gmail.com>
2023-11-26 20:12:15 +0100

    styles: añado estilos a  bookings details

--- src/componentsStyle/general/BookingDetailsStyled.jsx
+++ src/componentsStyle/general/BookingDetailsStyled.jsx
@@ -2,46 +2,176 @@ import styled from "styled-components";
 
 export const BookingDetailsStyled = styled.section`
   width: 90%;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   margin-top: 3em;
-  height: 792px;
+  height: auto;
   border-radius: 20px;
   display: flex;
 `;
 
-export const DetailsTextStyled = styled.div `
-width: 50%;
-height: 100%;
-padding: 3em;
-border: 1px solid red;
-display: flex;
-flex-direction: column;
-`
+export const DetailsTextStyled = styled.div`
+  width: 50%;
+  height: 100%;
+  padding: 3em;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
-export const DetailsInfoPersonStyled = styled.div `
-width: 100%;
-height: 30%;
-padding: 3em;
-border: 1px solid red;
-`
-export const DetailsInfoRoomStyled = styled.div `
-width: 100%;
-height: 30%;
-padding: 3em;
-border: 1px solid red;
-`
-export const DetailsInfoFacilitiesStyled = styled.div `
-width: 100%;
-height: 30%;
-padding: 3em;
-border: 1px solid red;
-`
+export const DetailsInfoPersonStyled = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 2em;
+  margin-bottom: 3em;
 
+  & img {
+    width: 156px;
+    height: 156px;
+    border-radius: 20px;
+  }
+  & h1 {
+    color: #212121;
+    font-weight: 600;
+    font-size: 2rem;
+  }
+  & h6 {
+    color: #799283;
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+  & svg {
+    color: #135846;
+    width: 20px;
+    height: 20px;
+  }
+  & .container-image-info {
+    display: flex;
+    gap: 2em;
+  }
 
-export const DetailsImageStyled = styled.div `
-width: 50%;
-height: 100%;
-border: 1px solid blue;
-display: flex;
-flex-direction: column;
-`
+  & .container-namebutton {
+    height: 156px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  & .container-phone {
+    display: flex;
+    align-items: center;
+    width: 200px;
+    gap: 1em;
+  }
+  & .container-phone__button {
+    border-radius: 12px;
+    width: 200px;
+    height: 40px;
+    background-color: #135846;
+    color: #ffffff;
+    font-size: 1.2rem;
+    font-weight: 500;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    gap: 1em;
+  }
+  & .container-phone__button__icon {
+    color: #ffffff;
+  }
+`;
+
+export const DetailsCheckStyled = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: space-between;
+  padding: 0em 3em 2em 0em;
+  gap: 3em;
+  border-bottom: 1px solid #6e6e6ec5;
+
+  & h6 {
+    color: #6e6e6e;
+    font-size: 1.2em;
+    font-weight: 500;
+    margin-bottom: 0.5em;
+  }
+  & h3 {
+    color: #212121;
+    font-size: 1.2em;
+    font-weight: 600;
+  }
+`;
+export const DetailsInfoRoomStyled = styled.div`
+  margin-top: 4em;
+  width: 100%;
+  height: 30%;
+  padding: 0em 3em 2em 0em;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  & .container-room {
+    display: flex;
+    width: 85%;
+    justify-content: space-between;
+    margin-bottom: 1em;
+    &__info {
+      h6 {
+        color: #6e6e6e;
+        font-size: 1.2em;
+        font-weight: 500;
+        margin-bottom: 0.5em;
+      }
+      h3 {
+        color: #212121;
+        font-size: 1.6em;
+        font-weight: 600;
+      }
+    }
+  }
+  p {
+    width: 100%;
+    color: #363636;
+    font-size: 1.2rem;
+  }
+`;
+export const DetailsInfoFacilitiesStyled = styled.div`
+  margin-top: 1em;
+  width: 100%;
+  height: 30%;
+  padding: 0em 3em 2em 0em;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+
+  & h6 {
+    color: #6e6e6e;
+    font-size: 1.2em;
+    font-weight: 500;
+    margin-bottom: 1em;
+  }
+
+  & .container-buttons {
+    display: flex;
+    gap: 3em;
+  }
+`;
+
+export const DetailsButtonfacilitiesStyled = styled.button`
+  padding: 1em;
+  border-radius: 5px;
+  border: none;
+  background-color: #ebf1ef;
+  color: #135846;
+  font-weight: 600;
+  font-size: 1.2rem;
+`;
+
+export const DetailsImageStyled = styled.div`
+  width: 50%;
+  height: 100%;
+  border: 1px solid blue;
+  display: flex;
+  flex-direction: column;
+`;
--- src/pages/BookingDetailsPage.jsx
+++ src/pages/BookingDetailsPage.jsx
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import bookings from "../../src/JSON/bookings.json";
 import { MainStyled } from "../componentsStyle/general/MainStyled";
 import {
   BookingDetailsStyled,
+  DetailsButtonfacilitiesStyled,
+  DetailsCheckStyled,
   DetailsImageStyled,
   DetailsInfoFacilitiesStyled,
   DetailsInfoPersonStyled,
   DetailsInfoRoomStyled,
   DetailsTextStyled,
 } from "../componentsStyle/general/BookingDetailsStyled";
+import bookings from "../../src/JSON/bookings.json";
+import { FaPhone } from "react-icons/fa6";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { SlOptionsVertical } from "react-icons/sl";
 
 const BookingDetailsPage = () => {
   const { id } = useParams();
   const [details, setDetails] = useState(null);
-  console.log(details);
+  // console.log(details);
 
   useEffect(() => {
     const searchBooking = bookings.find(
@@ -25,16 +30,70 @@ const BookingDetailsPage = () => {
 
   return (
     <MainStyled>
-      <BookingDetailsStyled>
-        <DetailsTextStyled>
-          <DetailsInfoPersonStyled></DetailsInfoPersonStyled>
-          <DetailsInfoRoomStyled></DetailsInfoRoomStyled>
-          <DetailsInfoFacilitiesStyled></DetailsInfoFacilitiesStyled>
-        </DetailsTextStyled>
-        <DetailsImageStyled>
-          
-        </DetailsImageStyled>
-      </BookingDetailsStyled>
+      {details && (
+        <BookingDetailsStyled>
+          <DetailsTextStyled>
+            <DetailsInfoPersonStyled>
+              <div className="container-image-info">
+                <img src={details.photo} alt="" />
+                <div className="container-namebutton">
+                  <h1>{details.name}</h1>
+                  <h6>{details.id}</h6>
+                  <div className="container-phone">
+                    <FaPhone />
+                    <button className="container-phone__button">
+                      <BiMessageSquareDetail className="container-phone__button__icon" />
+                      Send Message
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <SlOptionsVertical />
+            </DetailsInfoPersonStyled>
+            <DetailsCheckStyled>
+              <div className="box-check">
+                <h6>Check in</h6>
+                <h3>
+                  {details.checkin} | {details.checkinTime}
+                </h3>
+              </div>
+              <div className="box-check">
+                <h6>Check out</h6>
+                <h3>
+                  {details.checkout} | {details.checkoutTime}
+                </h3>
+              </div>
+            </DetailsCheckStyled>
+            <DetailsInfoRoomStyled>
+              <div className="container-room">
+                <div className="container-room__info">
+                  <h6>Room Info</h6>
+                  <h3>{details.Room}</h3>
+                </div>
+                <div className="container-room__info">
+                  <h6>Price</h6>
+                  <h3>
+                    ${details.price}
+                    <span> /night</span>
+                  </h3>
+                </div>
+              </div>
+              <p>{details.notes}</p>
+            </DetailsInfoRoomStyled>
+            <DetailsInfoFacilitiesStyled>
+              <h6>Facilities</h6>
+              <div className="container-buttons">
+                {details.facilities.map((item) => (
+                  <DetailsButtonfacilitiesStyled>
+                    {item}
+                  </DetailsButtonfacilitiesStyled>
+                ))}
+              </div>
+            </DetailsInfoFacilitiesStyled>
+          </DetailsTextStyled>
+          <DetailsImageStyled></DetailsImageStyled>
+        </BookingDetailsStyled>
+      )}
     </MainStyled>
   );
 };

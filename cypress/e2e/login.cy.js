a0df772a63635321ee42065fc54b420889e0ad63
omarPerez1987 <omar.perez.dev@gmail.com>
2023-12-06 20:21:10 +0100

    test: añado test end to end: comprobacion de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,9 +2,9 @@ describe("Comprobación de login", () => {
   it("Tiene que dirigir a /home/dashboard después de meter los datos correctos", () => {
     cy.visit("http://localhost:5173/");
 
-    cy.get("input[name=email]").type("test@test.com");
+    cy.get("input[type=email]").type("test@test.com");
 
-    cy.get("input[name=password]").type("9999");
+    cy.get("input[type=password]").type("9999");
 
     cy.get("button[type=submit]").click();
     cy.url().should("include", "/home/dashboard");
@@ -12,9 +12,9 @@ describe("Comprobación de login", () => {
   it("Tiene que quedarse en Login al introducir los datos incorrectos", () => {
     cy.visit("http://localhost:5173/");
 
-    cy.get("input[name=email]").type("test@test.com");
+    cy.get("input[type=email]").type("test@test.com");
 
-    cy.get("input[name=password]").type("2323");
+    cy.get("input[type=password]").type("2323");
 
     cy.get("button[type=submit]").click();
     cy.url().should("include", "/");

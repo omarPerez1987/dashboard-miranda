352623a9615b1f87fa0e276c808616eb7f48c0be
omarPerez1987 <omar.perez.dev@gmail.com>
2024-01-19 16:29:55 +0100

    cambios en mode para deploy

--- src/mode/debugMode.ts
+++ src/mode/debugMode.ts
@@ -1,9 +1,10 @@
-
+// export const getBaseUrl = (): string => {
+//   if (import.meta.env.MODE === 'development') {
+//     return import.meta.env.VITE_LOCAL_API_URL;
+//   } else {
+//     return import.meta.env.VITE_PRODUCTION_API_URL;
+//   }
+// };
 export const getBaseUrl = (): string => {
-  if (import.meta.env.MODE === 'development') {
-    return import.meta.env.VITE_LOCAL_API_URL;
-  } else {
-    return import.meta.env.VITE_PRODUCTION_API_URL;
-  }
+  return import.meta.env.VITE_PRODUCTION_API_URL;
 };
-
